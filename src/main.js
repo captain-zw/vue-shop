efc6b23636fc41c34a71ba5fb8f6076fddf322be
zhangwei <1849529379@qq.com>
2021-02-09 22:00:39 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+
+// 配置请求跟路径
+axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
+
+// 挂载axios到原型上
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
