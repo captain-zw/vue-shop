d6db59af0d268f09d112f34547057ccaff5585df
zhangwei <1849529379@qq.com>
2021-07-26 15:17:16 +0800

    7.26 start

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    分类参数
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    created() {
+
+    },
+    metheds: {}
+  }
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
