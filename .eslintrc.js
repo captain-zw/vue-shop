68e59b5ed20d4e94d51d6fc39df40674676b01bd
1849529379@qq.com <1849529379@qq.com>
2021-12-11 11:01:11 +0800

    完成订单功能开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 0,
+    'quotes': [0, 'double'],
     'indent': 'off'
   }
 }
